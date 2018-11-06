ed681cd148c36048f36a9b9d46e892ab67d056f2
cabomaldade <soutom@yahoo.com.br>
2018-11-05 22:33:03 -0300

    Initial commit

---
+++ app/src/main/java/quatro/integrador/projeto/com/projetointegradoriv2018/Activities/MainActivity.java
@@ -0,0 +1,23 @@
+package quatro.integrador.projeto.com.projetointegradoriv2018.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import quatro.integrador.projeto.com.projetointegradoriv2018.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mudaTela();
+    }
+
+    private void mudaTela() {
+        Intent int = new Intent();
+
+    }
+}
